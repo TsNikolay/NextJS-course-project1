e848dec527356df0ac0602aa358e0c796fb93a5f
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-12-01 17:04:35 +0100

    Basic structure of the pages was adeed

---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventDetailPage = () => {
+    return (
+        <div>
+            <h1>This is the Event Detail page</h1>
+        </div>
+    );
+};
+
+    export default EventDetailPage;
