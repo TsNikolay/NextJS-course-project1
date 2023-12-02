ec0676c279887a424ec31e1862cf1eac1226b44b
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-12-02 19:06:22 +0100

    Event page was implemented

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,31 @@
-import React from 'react';
-
+import React, { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 const EventDetailPage = () => {
-    return (
-        <div>
-            <h1>This is the Event Detail page</h1>
-        </div>
-    );
+  const router = useRouter();
+  const eventID = router.query.eventId;
+  const event = getEventById(eventID);
+
+  if (!event) {
+    return <h1>No event found</h1>;
+  }
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
-    export default EventDetailPage;
+export default EventDetailPage;
