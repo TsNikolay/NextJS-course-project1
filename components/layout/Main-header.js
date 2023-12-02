d3ae4b00526daea0ef225ef460a350708807058e
Nikolay <90026719+TsNikolay@users.noreply.github.com>
2023-12-02 19:23:46 +0100

    Simple navigation bar was added

---
+++ components/layout/Main-header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./Main-header.module.css";
+const MainHeader = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Link href="/">NextEvents</Link>
+      </div>
+      <nav className={styles.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">Browse all events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
